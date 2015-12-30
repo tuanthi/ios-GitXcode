8f50e0da6a8ac6e49cec9cb9a5950d7f01382218
TUAN THI <huetuan1984@gmail.com>
2015-12-30 10:58:07 -0500

    Initial Commit

---
+++ Git-xcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git-xcode
+//
+//  Created by HUE TUAN THI on 12/30/15.
+//  Copyright © 2015 HUE TUAN THI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
